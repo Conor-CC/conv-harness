160c588109c1514019ef261f78757b590e6c5c4a
Conor Clery <34569973+ConorClery@users.noreply.github.com>
2019-03-28 14:21:28 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from subprocess import call
 call(["gcc", "-O3", "-fopenmp",  "-msse4", "conv-harness-1.c"])
 call(["./a.out", "16", "16", "3", "64", "64"])
 call(["./a.out", "64", "64", "3", "128", "32"])
-call(["./a.out", "128", "64", "7", "32", "32"])
+call(["./a.out", "128", "64", "7", "32", "256"])
